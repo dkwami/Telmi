35bc844e69068e90978f02c6fb04b84eddf9f936
dkwami <delali.kwami@gmail.com>
2015-12-08 04:35:21 -0600

    [TLM2] Adding location column to Users table.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151127220223) do
+ActiveRecord::Schema.define(version: 20151208103007) do
 
   create_table "users", force: :cascade do |t|
     t.string   "username"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20151127220223) do
     t.datetime "updated_at",      null: false
     t.date     "birthday"
     t.string   "password_digest"
+    t.string   "location"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
