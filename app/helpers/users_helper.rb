2d706f54052f2fb70684e7ac7c3fdc0921f6d2dd
dkwami <delali.kwami@gmail.com>
2016-01-25 10:03:21 -0600

    Finishing TLM-4 Feed.

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,21 @@
 module UsersHelper
+
+  def age(id)
+    @now = Date.today
+    @user = User.find(id)
+     if @user.birthday.month < @now.month
+       @user_age = @now.year - @user.birthday.year
+         #print @user_age
+     elsif @user.birthday.month == @now.month
+       if @user.birthday.day <= @now.day
+         @user_age = @now.year - @user.birthday.year
+         #print @user_age
+       end
+     else
+         @user_age = @now.year - @user.birthday.year - 1
+         #print @user_age
+    end
+
+    #puts "here!"
+  end
 end
