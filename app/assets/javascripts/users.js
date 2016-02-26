dd0fcbb464892af57a65ed5d1b825505c23866f7
dkwami <delali.kwami@gmail.com>
2016-02-25 19:35:56 -0600

    Committing before revert.

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,13 +1,10 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
 // You can use CoffeeScript in this file: http://coffeescript.org/
+$('#recommendation-form').hide();
 
 $(document).ready(function(){
-  $('#user-recommendation-submit').hide();
-
-  $('#show-rec-form').click(myFunction() {
-    $('#user-recommendation-submit').toggle();
-    //formShow = $('#user-recommendation-submit').style.visibility;
-    //formShow = ((formShow == "hidden") ? "visible" : "hidden");
-  })
-})
+  $('#show-rec-form').click(function(){
+    $('#recommendation-form').show();
+  });
+});
--- app/models/recommendation.rb
+++ app/models/recommendation.rb
@@ -2,4 +2,5 @@ class Recommendation < ActiveRecord::Base
   belongs_to :user
   validates :title, presence: true
   validates :category, presence: true
+  validates :description, presence: true
 end
